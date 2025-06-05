f7db9b32ddedf6588f27c755eaecdbb59d546dce
krunoslav-k <krunoslavk@hotmail.com>
2025-06-05 22:15:45 +0200

    Add elements to JS and some functions

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let addButtonEl = document.querySelector(".addButton");
+const addButtonEl = document.querySelector(".add-button");
+const inputFieldEl = document.querySelector(".input-field");
+const toDoListEl = document.querySelector(".to-do-list");
+const toDoItemEl = document.createElement("li");
 
 function addToDo() {
-  console.log("provjera");
+  //console.log(inputFieldEl.value);
+  toDoItemEl.textContent = inputFieldEl.value;
+  toDoListEl.appendChild(toDoItemEl);
 }
 
 addButtonEl.addEventListener("click", addToDo);
