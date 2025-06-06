b314cb50b6f53f6db335923a704783353b5e88b1
krunoslav-k <krunoslavk@hotmail.com>
2025-06-06 18:32:31 +0200

    Complete addToDo function to append typed text to the list

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const addButtonEl = document.querySelector(".add-button");
 const inputFieldEl = document.querySelector(".input-field");
 const toDoListEl = document.querySelector(".to-do-list");
-const toDoItemEl = document.createElement("li");
+let toDoCounter = 0;
 
 function addToDo() {
-  //console.log(inputFieldEl.value);
-  toDoItemEl.textContent = inputFieldEl.value;
-  toDoListEl.appendChild(toDoItemEl);
+  toDoCounter++;
+
+  const toDoListItemEl = document.createElement("li");
+
+  const checkboxInputEl = document.createElement("input");
+  checkboxInputEl.type = "checkbox";
+  checkboxInputEl.id = `item--${toDoCounter}`;
+
+  const chechboxLabelEl = document.createElement("label");
+  chechboxLabelEl.htmlFor = `item--${toDoCounter}`;
+  chechboxLabelEl.textContent = ` ${inputFieldEl.value}`;
+
+  toDoListItemEl.appendChild(checkboxInputEl);
+  toDoListItemEl.appendChild(chechboxLabelEl);
+  toDoListEl.appendChild(toDoListItemEl);
+
+  inputFieldEl.value = "";
 }
 
 addButtonEl.addEventListener("click", addToDo);
