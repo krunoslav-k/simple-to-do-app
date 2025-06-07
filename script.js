95c500bcacb3038332798eaa027fc91e0ec11e98
krunoslav-k <krunoslavk@hotmail.com>
2025-06-07 09:53:09 +0200

    refactor: extract helper functions from addToDo

--- script.js
+++ script.js
@@ -5,27 +5,30 @@ let toDoCounter = 0;
 
 function addToDo() {
   toDoCounter++;
-
-  const toDoListItemEl = document.createElement("li");
-
-  const checkboxInputEl = document.createElement("input");
-  checkboxInputEl.type = "checkbox";
-  checkboxInputEl.id = `item--${toDoCounter}`;
-
-  const chechboxLabelEl = document.createElement("label");
-  chechboxLabelEl.htmlFor = `item--${toDoCounter}`;
-  chechboxLabelEl.textContent = ` ${inputFieldEl.value}`;
-
-  const deleteIconEl = document.createElement("img");
-  deleteIconEl.src = "./images/black delete icon.png";
-  deleteIconEl.classList.add("delete-icon");
-
-  toDoListItemEl.appendChild(checkboxInputEl);
-  toDoListItemEl.appendChild(chechboxLabelEl);
-  toDoListItemEl.appendChild(deleteIconEl);
+  const toDoListItemEl = createToDoListItem(toDoCounter);
   toDoListEl.appendChild(toDoListItemEl);
 
   inputFieldEl.value = "";
+}
+
+function createCheckboxInput(toDoCounter) {
+  const checkboxInputEl = document.createElement("input");
+  checkboxInputEl.type = "checkbox";
+  checkboxInputEl.id = `item--${toDoCounter}`;
+  return checkboxInputEl;
+}
+
+function createChechboxLabel(toDoCounter) {
+  const chechboxLabelEl = document.createElement("label");
+  chechboxLabelEl.htmlFor = `item--${toDoCounter}`;
+  chechboxLabelEl.textContent = ` ${inputFieldEl.value}`;
+  return chechboxLabelEl;
+}
+
+function createDeleteIcon() {
+  const deleteIconEl = document.createElement("img");
+  deleteIconEl.src = "./images/black delete icon.png";
+  deleteIconEl.classList.add("delete-icon");
 
   deleteIconEl.addEventListener("mouseover", () => {
     console.log(deleteIconEl);
@@ -36,10 +39,26 @@ function addToDo() {
     console.log(deleteIconEl);
     deleteIconEl.src = "./images/black delete icon.png";
   });
+
+  return deleteIconEl;
+}
+
+function createToDoListItem(toDoCounter) {
+  const toDoListItemEl = document.createElement("li");
+  chechboxInputEl = createCheckboxInput(toDoCounter);
+  chechboxLabelEl = createChechboxLabel(toDoCounter);
+  deleteIconEl = createDeleteIcon();
+
+  toDoListItemEl.appendChild(checkboxInputEl);
+  toDoListItemEl.appendChild(chechboxLabelEl);
+  toDoListItemEl.appendChild(deleteIconEl);
+
+  return toDoListItemEl;
 }
 
 addButtonEl.addEventListener("click", addToDo);
 
+//brisanje <li> klikom na delete ikonicu
 toDoListEl.addEventListener("click", function (e) {
   if (e.target.classList.contains("delete-icon")) {
     const li = e.target.closest("li");
